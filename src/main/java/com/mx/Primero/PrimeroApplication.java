c2dacd6d3faba4f187d03cbf50034ebc3e3dfcd7
Samuel Perez Zistecatl <samyzistec@gmail.com>
2024-11-19 01:42:46 -0600

    Primer commit

---
+++ src/main/java/com/mx/Primero/PrimeroApplication.java
@@ -0,0 +1,13 @@
+package com.mx.Primero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeroApplication.class, args);
+	}
+
+}
